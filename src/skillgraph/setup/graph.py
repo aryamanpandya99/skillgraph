e49bc05b9b684516685bfe5988b207019dcea374
aryamanpandya <aryaman.pandya@tufts.edu>
2024-11-28 10:21:47 -0500

    modified:   setup/graph.py

--- src/skillgraph/setup/graph.py
+++ src/skillgraph/setup/graph.py
@@ -1,8 +1,21 @@
 from pyvis.network import Network
 from skillgraph.setup.topics import TopicMap, generate_topic_breakdown, generate_topic_map
+from skillgraph.setup.graph import VisualGraph
+from typing import Dict, Tuple
 
 
-def build_graph_layers(root_topic: str) -> TopicMap:
+def build_graph_layers(root_topic: str) -> Tuple[Dict[str, TopicMap], Dict[str, VisualGraph]]:
+    """
+    Build a hierarchical graph of skill dependencies for a given root topic.
+    
+    Args:
+        root_topic (str): The root topic to build the graph from
+        
+    Returns:
+        tuple: A tuple containing:
+            - dict: Topic breakdowns mapping topic names to their breakdowns
+            - dict: Topic graphs mapping topic names to their visualizations
+    """
     topic_breakdown, topic_graph = build_graph(root_topic)
     subtopics = topic_breakdown.content[0].parsed.subtopics
     
@@ -20,7 +33,10 @@ def build_graph_layers(root_topic: str) -> TopicMap:
     return topic_breakdowns, topic_graphs
 
 
-def build_graph(topic_name: str, show: bool = True):
+def build_graph(topic_name: str, show: bool = True) -> Tuple[TopicMap, VisualGraph]:
+    """
+    Build a graph for a given topic.
+    """
     topic_breakdown = generate_topic_breakdown(topic_name)
     topics = topic_breakdown.content[0].parsed.subtopics
     topic_map = generate_topic_map(topics).content[0].parsed.mapping
@@ -52,6 +68,9 @@ class VisualGraph:
         )
         self.__title = title
         self._build_graph()
+
+    def __repr__(self) -> str:
+        return self.graph
     
     @property
     def title(self):
